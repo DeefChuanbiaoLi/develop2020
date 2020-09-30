dad78daf1b3b36b92d4d0fec6ef6575a428171aa
DeefChuanbiaoLi <71057814+DeefChuanbiaoLi@users.noreply.github.com>
2020-09-30 11:31:57 +0800

    Update Trapezoid.java
    reason

--- teamwork/src/teamwork/Trapezoid.java
+++ teamwork/src/teamwork/Trapezoid.java
@@ -15,6 +15,7 @@ public class Trapezoid extends ABS_Pattern {
 	@Override
 	public double Area(double length, double width, double height, double TopBottom, double Bottom) {
 		// TODO Auto-generated method stub
+	System.out.println("55555");
 		return (TopBottom+Bottom)*height/2.0;
 	}
 
